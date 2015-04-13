c81a9504452aaabf14e6f83a788b2ed470bb24bd
sanjsanj <sanjsanj@hotmail.com>
2015-04-13 13:03:27 +0100

    Knows number is divisible by 5

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -20,10 +20,18 @@ describe ('Javabuzz', function() {
 
   });
 
-  describe ('knows when a number is', function(){
+  describe('knows when a number is', function(){
     it('divisible by 5', function() {
       javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.divisibleByFive(4)).toBe(false);
     });
 
   });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -2,4 +2,10 @@ var Javabuzz = function () {};
 
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return (number % 3 === 0);
+
+};
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+
 };
