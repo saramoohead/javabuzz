d2f07f37243dbd3081d8e2624f8a9917ffe516cd
sanjsanj <sanjsanj@hotmail.com>
2015-04-13 12:41:33 +0100

    Knows when number is divisible by 3, but not when it's not

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,9 +2,22 @@ describe ('Javabuzz', function() {
 
   var javabuzz;
 
-  describe('knows when a number is', function(){ 
+  describe('knows when a number is', function() {
+
     it('divisible by 3', function() {
       javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
+
   });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(2)).toBe(false);
+    });
+
+  });
+
 });
