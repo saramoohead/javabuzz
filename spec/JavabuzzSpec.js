408ddeefda1c4ea2fd24aec307d46308f2bc749e
sanjsanj <sanjsanj@hotmail.com>
2015-04-13 14:51:24 +0100

    Implement says functions that determines the correct return-output for the game

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -37,8 +37,26 @@ describe ('Javabuzz', function() {
   describe('it knows when a number is', function () {
     it('divisible by 15', function(){
       expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
-    }); 
+    });
 
+  });
+
+  describe('it knows it is NOT', function () {
+    it('divisible by 15', function(){
+      expect(javabuzz.isDivisibleByFifteen(13)).toBe(false);
+    });
+  });
+
+  describe('when playing, says', function() {
+    it('"Java" when a number is divisible by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+    it('"JavaBuzz" when a number is divisible by 15', function() {
+      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+    });
   });
 
 });
